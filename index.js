e2ff9055685be742309554f1568903a5e50d3c3e
Daniel Nielson <daniel@unfairsurprisery.org>
2017-11-03 21:16:26 -0600

    Initial version of app. Turns images into imgur embeds.

---
+++ index.js
@@ -0,0 +1,22 @@
+var App = require('ghost-app')
+  , GhostMdImgur;
+
+GhostMdImgur = App.extend({
+  filters: {
+    prePostsRender: 'handlePrePostsRender'
+  },
+  handlePrePostsRender: function handlePrePostsRender(payload) {
+    let modified = Object.assign({},payload);
+    modified.html = modified.html.replace(
+      /<img src="https?:\/\/(www.)?imgur.com\/(gallery\/)?([a-zA-Z0-9]{7})\/?" alt="imgur">/g,
+      '<blockquote class="imgur-embed-pub" lang="en" data-id="$3"><a href="//imgur.com/$3">View on Imgur</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>'
+    ).replace(
+      /<img src="https?:\/\/(www.)?imgur.com\/(gallery\/|a\/)?([a-zA-Z0-9]{5})\/?" alt="imgur">/g,
+      '<blockquote class="imgur-embed-pub" lang="en" data-id="a/$3"><a href="//imgur.com/a/$3">View on Imgur</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>'
+    );
+
+    return modified;
+  }
+});
+
+module.exports = GhostMdImgur;
