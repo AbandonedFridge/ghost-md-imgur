225d81a899d4126e7fe47923dca5d7f71d161260
Daniel Nielson <daniel@unfairsurprisery.org>
2017-11-05 12:11:57 -0700

    Allow alt to be whatever.

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ GhostMdImgur = App.extend({
   handlePrePostsRender: function handlePrePostsRender(payload) {
     let modified = Object.assign({},payload);
     modified.html = modified.html.replace(
-      /<img src="https?:\/\/(www.)?imgur.com\/(gallery\/)?([a-zA-Z0-9]{7})\/?" alt="imgur">/g,
-      '<blockquote class="imgur-embed-pub" lang="en" data-id="$3"><a href="//imgur.com/$3">View on Imgur</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>'
+      /<img src="https?:\/\/(www.)?imgur.com\/(gallery\/)?([a-zA-Z0-9]{7})\/?" alt="(.*)">/g,
+      '<blockquote class="imgur-embed-pub" lang="en" data-id="$3"><a href="//imgur.com/$3">$4</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>'
     ).replace(
-      /<img src="https?:\/\/(www.)?imgur.com\/(gallery\/|a\/)?([a-zA-Z0-9]{5})\/?" alt="imgur">/g,
-      '<blockquote class="imgur-embed-pub" lang="en" data-id="a/$3"><a href="//imgur.com/a/$3">View on Imgur</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>'
+      /<img src="https?:\/\/(www.)?imgur.com\/(gallery\/|a\/)?([a-zA-Z0-9]{5})\/?" alt="(.*)">/g,
+      '<blockquote class="imgur-embed-pub" lang="en" data-id="a/$3"><a href="//imgur.com/a/$3">$4</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>'
     );
 
     return modified;
